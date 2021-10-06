c0702b04f1bbf1af6e282b6b3437e43ed01688b0
unknown <vladancupric@gmail.com>
2021-10-06 10:56:57 +0200

    Core version

--- src/RecommendedVideo.js
+++ src/RecommendedVideo.js
@@ -71,6 +71,39 @@ function RecommendedVideo() {
                 channel="Sonny Netflix"
                 image = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
                 />
+                 <VideoCard 
+                title="Beacome a Web Developer in 10 minutes | 2021"
+                views ="2.4M Views"
+                timestamp = "3 days ago"
+                channelImage = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                channel="Sonny Netflix"
+                image = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                />
+                 <VideoCard 
+                title="Beacome a Web Developer in 10 minutes | 2021"
+                views ="2.4M Views"
+                timestamp = "3 days ago"
+                channelImage = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                channel="Sonny Netflix"
+                image = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                />
+                 <VideoCard 
+                title="Beacome a Web Developer in 10 minutes | 2021"
+                views ="2.4M Views"
+                timestamp = "3 days ago"
+                channelImage = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                channel="Sonny Netflix"
+                image = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                />
+                 <VideoCard 
+                title="Beacome a Web Developer in 10 minutes | 2021"
+                views ="2.4M Views"
+                timestamp = "3 days ago"
+                channelImage = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                channel="Sonny Netflix"
+                image = "https://www.danilovesovic.com/images/course_images/isrn3Pakm2DTq0sLtjou0X5GJs0eigTgRGL3u9fQ.jpeg"
+                />
+
                 
             </div>
 
