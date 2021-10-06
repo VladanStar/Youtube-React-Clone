0ac0a1d2090e4519cd854048a0cd8a9aa8de8953
unknown <vladancupric@gmail.com>
2021-10-06 21:52:51 +0200

    New React Commit

---
+++ src/VideoRow.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./VideoRow.css"
+
+function VideoRow({
+    views,
+     subs,
+     description, 
+     timestamp,
+     channel,
+     title,
+     image
+    }) {
+    return (
+        <div className="videoRow">
+            <img src={image} alt=""/>
+            <div className="videoRow_text">
+                <h3>{title}</h3>
+                <p className="videoRow_headline">
+                    {channel} * 
+                    <span className="videoRow_subs">
+                    <span className="videoRow_subsNumber">{subs}</span>
+                     Subscribers   </span>{" "}
+                     {views} views * {timestamp}
+                    
+                  
+                    </p>
+            <p className="videoRow_description">
+                {description}</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default VideoRow;
